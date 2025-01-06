a769691ab97bebfa6267dd70afa1e9eb8c63260a
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2025-01-05 19:08:03 -0700

    added loading prop to button

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -1,26 +1,26 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
-import { ArrowCircleLeft, ArrowCircleRight  } from 'iconsax-react';
-
+import { ArrowCircleLeft, ArrowCircleRight } from "iconsax-react";
 
 // Default metadata of the story https://storybook.js.org/docs/react/api/csf#default-export
 const meta: Meta<typeof Button> = {
-    title: "Atoms/Button",
-    component: Button,
-    argTypes: {
-         variant: {
-            control: "select",
-            options: ["primary", "soft", "destructive", "outline", 'link'],
-            description: "The visual style of the button"
-        },
-        size: {
-            control: "select",
-            options: ["sm", "md", "lg", "xl"],
-            description: "The size of the button"
-        },
-        disabled: { control: "boolean" },
-        children: { control: "text" },
+  title: "Atoms/Button",
+  component: Button,
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["primary", "soft", "destructive", "outline", "link"],
+      description: "The visual style of the button",
     },
+    size: {
+      control: "select",
+      options: ["sm", "md", "lg", "xl"],
+      description: "The size of the button",
+    },
+    disabled: { control: "boolean" },
+    children: { control: "text" },
+    loading: { control: "boolean" },
+  },
 };
 
 export default meta;
@@ -29,26 +29,22 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
-    args: {
-        variant: "primary",
-        size: "md",
-        children: (
-            <>
-                <ArrowCircleLeft variant="Bold" />
-                Button
-                <ArrowCircleRight variant="Bold" />
-            </>
-        )
-    },
+  args: {
+    variant: "primary",
+    size: "md",
+    children: (
+      <>
+        <ArrowCircleLeft variant="Bold" />
+        Button
+        <ArrowCircleRight variant="Bold" />
+      </>
+    ),
+  },
 };
 export const Icon: Story = {
-    args: {
-        variant: "primary",
-        size: "icon",
-        children: <ArrowCircleLeft variant="Bold" />
-    },
+  args: {
+    variant: "primary",
+    size: "icon",
+    children: <ArrowCircleLeft variant="Bold" />,
+  },
 };
-
-
-
-
